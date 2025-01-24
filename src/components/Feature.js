17dd784474345438c3cc0b6b2116df2ea1b842fc
Ashok Budha <abudha592@gmail.com>
2025-01-25 05:19:36 +0545

    add Feature section

---
+++ src/components/Feature.js
@@ -0,0 +1,20 @@
+
+
+const Feature = (props) =>{
+   const {image, title, description} = props.data;
+
+    return (
+        <div className="feature" id="feature1">
+                <div className="feature-illustration">
+                    <img src={image.src} alt={image.alt}/>
+                </div>
+                <div className="feature-content">
+                    <h1 className="feature-title">{title}</h1>
+                    <p className="feature-description">{description}</p>
+                    <a href="" className="feature-learn-more-btn btn">Learn More</a>
+                </div>
+            </div>
+    ) 
+}
+
+export default Feature;
